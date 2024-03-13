a5c8e1d19af97432a41e53445ebe8ca6868ded24
Valiyulloh0221 <lvov11190@gmail.com>
2024-03-13 13:50:13 +0500

    nimadur

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { list } from "postcss";
+import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  const handleForm = (e) => {
+    e.preventDefault();
+    setTodoList([...todoList, {todoName: todo}]);
+    setTodo("");
+  };
+  console.log(todoList);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-gray-200 w-full h-screen flex items-center">
+      <div className="text-center w-[400px] bg-white p-5 mx-auto">
+        <h1 className="text-5xl mb-8 font-bold">Todo List</h1>
+        <form onSubmit={handleForm}>
+          <input value={todo} onChange={(e) => setTodo(e.target.value)} className="border-2 border-black w-full placeholder:text-gray-500 rounded-lg p-5 mb-5 text-black" type="text" placeholder="Add Todo" id="" />
+          <button type="submit" className="bg-red-600 text-white py-3 px-8 rounded-lg mb-8">Add Todo</button>
+        </form>
+        <div className="todo-show-area">
+          <ul>
+            {todoList.map((singleTodo) => {
+              <li className="bg-black py-5 rounded-lg text-2xl px-5 flex justify-between text-white">
+                {singleTodo.todoName}{""} <span className="text-red-600 cursor-pointer">x</span>
+              </li>
+            })}
+          </ul>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
